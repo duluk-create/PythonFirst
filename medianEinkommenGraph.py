d5161bb2132bf06b741e26f314c19e8efe7f152e
duluk <96245211+duluk-create@users.noreply.github.com>
2022-09-01 13:54:46 +0200

    matplotlib Übung

--- medianEinkommenGraph.py
+++ medianEinkommenGraph.py
@@ -7,6 +7,8 @@ dev_y = [38296, 42000, 46752, 49320, 53200, 56000, 62316, 64928, 67317, 68748, 7
 py_dev_y = [45296, 48000, 53752, 54320, 60200, 65000, 67316, 68828, 69317, 78648, 83752]
 js_dev_y = [2296, 38000, 43752, 54320, 60200, 75000, 87316, 98828, 109317, 110048, 112752]
 
+#Datensätze
+
 plt.plot(ages_x, dev_y, linestyle='--', marker='.', label='All Devs')
 plt.plot(ages_x, js_dev_y, color='#adad3b', label='JavaScript', linewidth=3)
 plt.plot(ages_x, py_dev_y, marker='o', label='Python')
